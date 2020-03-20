90bf9540e11b7d2297f3005d72bc75fed565771b
Kostiantyn Pshenychnyi <konst8403@gmail.com>
2020-03-20 10:12:09 +0200

    Replace line edit path line with list path line That allows to navigate quickly

--- widget.cpp
+++ widget.cpp
@@ -2,6 +2,7 @@
 #include "ui_widget.h"
 #include <QDebug>
 #include <chrono>
+#include <iostream>
 
 using namespace std::literals;
 
@@ -38,28 +39,40 @@ void Widget::dir_selection_changed(const QItemSelection& selected, const QItemSe
 
 void Widget::on_dir_content_doubleClicked(const QModelIndex& index)
 {
-    QFileInfo entity_info = file_system->fileInfo(index);
+    QString file_name = file_system->fileName(index);
 
-    if (entity_info.isDir())
+    if (file_system->isDir(index))
     {
         ui->dir_content->setRootIndex(index);
 
-        ui->path_line->setText(entity_info.absoluteFilePath());
+        ui->path_line->addItem(path_line_prefix + file_name);
+
+        path.push_back(index);
     }
     else
     {
-        qDebug() << entity_info.path() << " is not a directory";
+        qDebug() << file_name<< " is not a directory";
     }
+
+    // clear selection
+    ui->dir_content->setCurrentIndex(QModelIndex());
 }
 
 void Widget::on_back_clicked()
 {
     QModelIndex current_dir_index = ui->dir_content->rootIndex();
     QModelIndex parent_dir_index = current_dir_index.parent();
-    QFileInfo   parent_dir_info = file_system->fileInfo(parent_dir_index);
 
     ui->dir_content->setRootIndex(parent_dir_index);
-    ui->path_line->setText(parent_dir_info.absoluteFilePath());
+
+    if (ui->path_line->count() > 0)
+    {
+        QListWidgetItem* last_item = ui->path_line->takeItem(ui->path_line->count() - 1);
+
+        delete last_item;
+
+        path.pop_back();
+    }
 
     // clear selection
     ui->dir_content->setCurrentIndex(QModelIndex());
@@ -71,16 +84,36 @@ void Widget::on_forward_clicked()
 
     if (selected_index.isValid())
     {
-        QFileInfo entity_info = file_system->fileInfo(selected_index);
+        QString file_name = file_system->fileName(selected_index);
 
-        if (entity_info.isDir())
+        if (file_system->isDir(selected_index))
         {
             ui->dir_content->setRootIndex(selected_index);
 
-            ui->path_line->setText(entity_info.absoluteFilePath());
+            ui->path_line->addItem(path_line_prefix + file_name);
+
+            path.push_back(selected_index);
         }
     }
 
      // clear selection
     ui->dir_content->setCurrentIndex(QModelIndex());
+}
+
+void Widget::on_path_line_itemClicked(QListWidgetItem*)
+{
+    auto current_row = ui->path_line->currentRow();
+    QModelIndex new_index = path[current_row];
+
+    ui->dir_content->setRootIndex(new_index);
+
+    path.erase(path.begin() + current_row + 1, path.end());
+
+    while (ui->path_line->count() - 1 != current_row)
+    {
+        QListWidgetItem* end_item = ui->path_line->takeItem(ui->path_line->count() - 1);
+        delete end_item;
+    }
+
+    ui->path_line->clearSelection();
 }
--- widget.h
+++ widget.h
@@ -2,10 +2,12 @@
 #define WIDGET_H
 
 #include <memory>
+#include <deque>
 
 #include <QWidget>
 #include <QFileSystemModel>
 #include <QItemSelection>
+#include <QListWidget>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -24,17 +26,21 @@ private slots:
 
     void dir_selection_changed(const QItemSelection& prev, const QItemSelection& next);
 
-    void on_dir_content_doubleClicked(const QModelIndex &index);
+    void on_dir_content_doubleClicked(const QModelIndex& index);
 
     void on_back_clicked();
 
     void on_forward_clicked();
+
+    void on_path_line_itemClicked(QListWidgetItem* item);
 
 private:
 
     Ui::Widget*                         ui;
     std::unique_ptr<QFileSystemModel>   file_system;
 
+    std::deque<QModelIndex>             path;
+    const QString                       path_line_prefix = " 🠚 ";
 };
 
 #endif // WIDGET_H
